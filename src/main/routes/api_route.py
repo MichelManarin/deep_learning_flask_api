992bd805ca5a3468fae2c4c135ae5eaf00cb7d7d
Michel Manarin <michelmanarin@gmail.com>
2024-04-13 16:56:19 -0300

    refactor: remove unnecessary imports

--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,6 +1,3 @@
-import base64
-from io import BytesIO
-from PIL import Image
 from flask import Blueprint, jsonify, request
 from src.infra.neural_network import Model
 
@@ -62,31 +59,14 @@ def register_detection_result():
         response.status_code,
     )
 
-
-
-@api_routes_bp.route('/detect', methods=['POST'])
-def detect():
-    image_base64  = request.json['image_base64']
-    confidence = request.json['confidence']
-    iou = request.json['iou']
-    
-    image_bytes = base64.b64decode(image_base64)
-    image_buffer = BytesIO(image_bytes)
-    image = Image.open(image_buffer)
-    image_rgb = image.convert('RGB')
-    predictions = model(image_rgb, confidence, iou)
-    detections = [p.to_dict() for p in predictions]
-    
-    return jsonify(detections)
-
 # TODO: refactor endpoints below in future
-@api_routes_bp.route('/health_check', methods=['GET'])
+@api_routes_bp.route('/api/health_check', methods=['GET'])
 def health_check():
     if model is None:
         return "Model is not loaded"
     return f"Model {model.model_name} is loaded"
 
-@api_routes_bp.route('/load_model', methods=['POST'])
+@api_routes_bp.route('/api/load_model', methods=['POST'])
 def load_model():
     model_name = request.json['model_name']
     global model
