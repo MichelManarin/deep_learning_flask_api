fb772e8ce302edf1bc4bf89c97c7b26fd451b702
Michel Manarin <michelmanarin@gmail.com>
2024-04-12 22:19:34 -0300

    test: ensure select user_input test

--- tests/user_input_repository_test.py
+++ tests/user_input_repository_test.py
@@ -1,6 +1,7 @@
 import random
 from faker import Faker
 from src.infra.config import DBConnectionHandler
+from src.infra.entities import UserInput as UserInputModel
 from src.infra.repo.user_input_repository import UserInputRepository
 
 faker = Faker()
@@ -28,3 +29,25 @@ def test_insert_user_input():
   assert new_user_input.id == query_user_input.id
   assert new_user_input.confidence == query_user_input.confidence
   assert new_user_input.iou == query_user_input.iou
+
+def test_select_user_input():
+   """ Should select a user in UserInputs table and compare it """
+
+   user_input_id = 99999
+   path = faker.word()
+   confidence = fake_random_float()
+   iou = fake_random_float()
+   data = UserInputModel(id=user_input_id, confidence=confidence, iou=iou, video_path=path)
+
+   engine = db_connection_handler.get_engine()
+   engine.execute(
+      "INSERT INTO user_inputs (id, confidence, iou, video_path) VALUES ('{}', '{}', '{}', '{}');".format(
+         user_input_id, confidence, iou, path
+      )
+   )
+
+   query_user_input = user_input_repository.select(user_input_id=user_input_id)
+
+   assert data in query_user_input
+
+   engine.execute("DELETE FROM user_inputs WHERE id='{}';".format(user_input_id))
